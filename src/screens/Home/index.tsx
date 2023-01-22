8b68f8fb6f1accba479734b327a77224e4354ea7
BereniceQueiroz <baqueiroz14@gmail.com>
2023-01-21 21:44:59 -0300

    feat: add navigation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,7 @@
+
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { RFValue} from "react-native-responsive-fontsize";
 import LogoSVG from "~/assets/logo.svg";
 import { Car } from "~/components/Car";
@@ -13,6 +15,8 @@ import {
 } from './styles';
 
 export function Home(){
+  const navigation = useNavigation();
+
   const carData = {
     brand: 'Audi',
     name: 'RS 5 Coupé',
@@ -21,6 +25,10 @@ export function Home(){
       price: 120,
     },
     thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png',
+  }
+
+  function handleCarDetails() {
+    navigation.navigate('CarDetails');
   }
 
   return (
@@ -36,7 +44,7 @@ export function Home(){
       <CarList
         data={[1,2,3,4,5,6,7,8]}
         keyExtractor={item => String(item)}
-        renderItem={({item }) => <Car data={carData} /> }
+        renderItem={({item }) => <Car data={carData} onPress={handleCarDetails} /> }
       />
     </Container>
   );
