363fb0523541e415629134574c89e15b0b4eae54
BereniceQueiroz <baqueiroz14@gmail.com>
2023-04-23 16:11:02 -0300

    feat: slider images

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import {
   Container,
@@ -7,23 +7,51 @@ import {
   CarImageWrapper,
   CarImage
 } from './styles';
+import { FlatList, ViewToken } from 'react-native';
 
 interface ImageProps {
   imagesUrl:  string[]
 }
 
+interface ChangeImageProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
 export function ImageSlider({imagesUrl}: ImageProps){
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const indexChanged = useRef((info: ChangeImageProps) => {
+    const index = info.viewableItems[0].index!;
+    setImageIndex(index);
+  })
+
   return (
     <Container>
       <ImageIndexes>
-        <ImageIndex active={true} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
+      {
+        imagesUrl.map((_, index) => (
+          <ImageIndex
+            key={String(index)}
+            active={index === imageIndex}
+          />
+        ))
+      }
       </ImageIndexes>
-      <CarImageWrapper>
-        <CarImage source={{ uri: imagesUrl[0]}} resizeMode="contain" />
-      </CarImageWrapper>
+
+        <FlatList
+          data={imagesUrl}
+          keyExtractor={key => key}
+          renderItem={({item}) => (
+            <CarImageWrapper>
+              <CarImage source={{ uri: item }} resizeMode="contain" />
+            </CarImageWrapper>
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          onViewableItemsChanged={indexChanged.current}
+        />
+
     </Container>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -54,7 +54,7 @@ export function Home(){
       <Header>
         <HeaderContent>
           <LogoSVG width={RFValue(108)} height={RFValue(12)} />
-          <TotalCars>Total de 12 carros</TotalCars>
+          <TotalCars>Total de {cars.length} carros</TotalCars>
         </HeaderContent>
       </Header>
 
